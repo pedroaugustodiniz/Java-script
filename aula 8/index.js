4dbddcae9e3f9db02f6556605153c1bc4dd0affa
Pedro Augusto Pereira Diniz <154288749+pedroaugustodiniz@users.noreply.github.com>
2024-07-16 17:37:50 -0300

    inicio js

---
+++ aula 8/index.js
@@ -0,0 +1,11 @@
+const nome = 'Pedro Augusto';
+const sobrenome = 'Pereira';
+const idade = 20;
+const peso = 70;
+const altura = 1.74;
+let imc;
+let anoNascimento;
+imc = peso / (altura * altura);
+anoNascimento = 2024 - idade;
+console.log(imc, anoNascimento);
+
