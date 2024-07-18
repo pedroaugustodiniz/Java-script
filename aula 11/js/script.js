76fcc344156ee76f03946f6775aa9e313181ab03
Pedro Augusto Pereira Diniz <154288749+pedroaugustodiniz@users.noreply.github.com>
2024-07-17 21:05:55 -0300

    aula 11

---
+++ aula 11/js/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('Digite um numero')
+
+let num2 = prompt('Digite outro numero')
+num1 = Number(num1)
+num2 = Number(num2)
+const resultadoDaSoma = num1 + num2
+alert(`O resultado da soma foi  ${resultadoDaSoma}`)
